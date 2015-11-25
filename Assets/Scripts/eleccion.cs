43330ce2dff96ede885df063314c8dce10bbf1d7
jcastilla <killofsfsfs@hotmail.com>
2015-11-25 22:10:41 +0100

    PlanetasRA
    Juego Realidad aumentada en Unity

---
+++ Assets/Scripts/eleccion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eleccion : MonoBehaviour {
+
+	public void Jugar()
+	{
+		Application.LoadLevel("planetas");
+	}
+
+	public void Creditos()
+	{
+		Application.LoadLevel("creditos");
+	}
+
+	public void Salir()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/ordenPlanetas.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ordenPlanetas : MonoBehaviour {
+
+	public GameObject sol;
+	public GameObject tierra;
+	public GameObject jupiter;
+	public GameObject neptuno;
+	Text text;
+	float posSol;
+	float posTierra;
+	float posJupiter;
+	float posNeptuno;
+	bool cambioColor = false;
+
+	void Awake()
+	{
+		text = GetComponent <Text> ();
+		text.color = Color.red;
+
+		//puntos = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		posSol = sol.GetComponent<Transform> ().position.x;
+		posTierra = tierra.GetComponent<Transform> ().position.x;
+		posJupiter = jupiter.GetComponent<Transform> ().position.x;
+		posNeptuno = neptuno.GetComponent<Transform> ().position.x;
+
+		if (posSol > posTierra && posTierra > posJupiter && posJupiter > posNeptuno)
+			text.color = Color.green;
+	}
+}
